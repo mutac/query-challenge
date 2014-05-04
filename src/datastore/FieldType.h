27f4ecbaa75e4736bc3139068bde923e652cae6f
Matthew Waddell <mewtus@gmail.com>
2014-05-04 11:11:01 -0700

    Added project structure, and a few utility libraries

---
+++ src/datastore/FieldType.h
@@ -0,0 +1,16 @@
+
+#ifndef __FIELD_TYPES_H__
+#define __FIELD_TYPES_H__
+
+namespace DataStore
+{
+  typedef enum
+  {
+    FieldType_Text,
+    FieldType_Float,
+    FieldType_Date,
+    FieldType_Time
+  } FieldType;
+}
+
+#endif
---
+++ src/import/main.cpp
@@ -0,0 +1,44 @@
+/** Imports a bar separated text file into a datastore.
+*/
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <datastore\IField.h>
+
+static const char kFieldDelimiter = '|';
+
+int main(int argc, char** argv)
+{
+  try
+  {
+    TCLAP::CmdLine cmd("Datastore Importer. Reads bar-delimited input and appends records to datastore.", ' ');
+    TCLAP::ValueArg<std::string> schemeArg("s", "scheme", "JSON Datastore scheme filename", false, "Scheme.json", "JSON scheme filename");
+
+    cmd.add(schemeArg);
+    cmd.parse(argc, argv);
+
+    //
+    // Read records from stdin
+    //
+
+    std::string header;
+    std::cin >> header;
+
+    for (std::string row; std::getline(std::cin, row);)
+    {
+      std::istringstream tokenStream(row);
+      for (std::string field; std::getline(tokenStream, field, kFieldDelimiter);)
+      {
+        std::cout << field << "   ";
+      }
+      std::cout << std::endl;
+    }
+  }
+  catch (TCLAP::ArgException &e)
+  {
+    std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;
+  }
+
+	return 0;
+}
